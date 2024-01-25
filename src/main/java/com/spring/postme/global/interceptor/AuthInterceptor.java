50a36edf9c59c82a8139535762174b0185783a08
rhrn456 <knbng@naver.com>
2024-01-25 13:37:44 +0900

    interceptor 설정 환료(게시글 작성, 댓글작성)

--- src/main/java/com/spring/postme/global/interceptor/AuthInterceptor.java
+++ src/main/java/com/spring/postme/global/interceptor/AuthInterceptor.java
@@ -1,5 +1,44 @@
 package com.spring.postme.global.interceptor;
 
-public class AuthInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AuthInterceptor implements HandlerInterceptor{
+
+	
+	
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)throws Exception {
+		
+		HttpSession session = request.getSession(false);
+		
+		
+		if(session == null || session.getAttribute("user") == null) {
+			response.sendRedirect("/login");
+			return false;
+		}
+			return true;
+	}
+	@Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+        
+        System.out.println("postHandle1");
+        
+        
+    }
+ 
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        
+        System.out.println("afterCompletion1");
+        
+        
+    }    
 }
