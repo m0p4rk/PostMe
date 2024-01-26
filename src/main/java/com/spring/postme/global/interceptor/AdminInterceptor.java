8416d4f2bf5545ac93b33e09744e62a52d1ce18d
rhrn456 <knbng@naver.com>
2024-01-26 11:03:07 +0900

    언터셉터 어드민과 유저 분리후 적용

--- src/main/java/com/spring/postme/global/interceptor/AdminInterceptor.java
+++ src/main/java/com/spring/postme/global/interceptor/AdminInterceptor.java
@@ -7,23 +7,23 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-public class AuthInterceptor implements HandlerInterceptor{
+public class AdminInterceptor implements HandlerInterceptor{
 
 	
 	
 	
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)throws Exception {
-		
-		HttpSession session = request.getSession(false);
-		
-		
-		if(session == null || session.getAttribute("user") == null) {
-			response.sendRedirect("/login");
-			return false;
-		}
-		  
-			return true;
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	    HttpSession session = request.getSession(false);
+
+	    // 관리자인지 확인
+	    if (session.getAttribute("isAdmin") == null || session.getAttribute("isAdmin").equals(false) || session.getAttribute("isAdmin").equals(0)) {
+	        // 관리자가 아닌 경우 로그인 페이지로 리다이렉트
+	        response.sendRedirect("/login");
+	        return false;
+	    }
+
+	    return true;
 	}
 	@Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
